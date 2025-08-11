20dc6a0d890a06cc710114649b4ea0e6a5a974c5
nabeel78-lab <nabeel.23csp55@student.uomosul.edu.iq>
2025-08-11 18:38:46 +0300

    ADD detect self Id
    Signed-off-by: nabeel78-lab <nabeel.23csp55@student.uomosul.edu.iq>

--- orderer/consensus/bdls/consenter.go
+++ orderer/consensus/bdls/consenter.go
@@ -1,9 +1,18 @@
 package bdls
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/IBM/idemix/common/flogging"
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
+	"github.com/hyperledger/fabric/common/crypto"
+	"github.com/hyperledger/fabric/orderer/common/cluster"
+)
 
 type Consenter struct {
-	Logger   string
+	Comm     *cluster.AuthCommMgr
+	Logger   *flogging.FabricLogger
 	Identity string
 	Config   string
 }
@@ -16,9 +25,22 @@ func New(logger, identity, config string) *Consenter {
 
 	return &Consenter{
 
-		Logger:   logger,
 		Identity: identity,
 		Config:   config,
 	}
 
+}
+
+func (c *Consenter) detectSelfID(consenters []*common.Consenter) (uint32, error) {
+	for _, cst := range consenters {
+		santizedCert, err := crypto.SanitizeX509Cert(cst.Identity)
+		if err != nil {
+			return 0, err
+		}
+		if bytes.Equal(c.Comm.NodeIdentity, santizedCert) {
+			return cst.Id, nil
+		}
+	}
+	c.Logger.Warning("Could not find the node in channel consenters set")
+	return 0, cluster.ErrNotInChannel
 }
